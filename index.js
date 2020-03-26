7ca16741a3d834a4176501207184e1a0d18abd89
robertmaier <robert.maier@redbull.com>
2020-03-26 11:25:17 +0100

    adds prNumber replacement

--- index.js
+++ index.js
@@ -2,12 +2,12 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 
 try {
-  const prId = core.getInput('pr-id');
-  console.log(`PR ID is ${prId}`);
-  core.setOutput('pr-id', prId);
   // Get the JSON webhook payload for the event that triggered the workflow
   const payload = JSON.stringify(github.context.payload, undefined, 2)
-  console.log(`The event payload: ${payload}`);
+  const prNumber = payload.number;
+  console.log(`PR Number is ${prNumber}`);
+  const pullRequestBody = payload.pull_request.body.replace(/#{prNumber}/g, prNumber);
+  core.setOutput('body', pullRequestBody);
 } catch (error) {
   core.setFailed(error.message);
 }
